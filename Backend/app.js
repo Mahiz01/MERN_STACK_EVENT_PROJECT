154a24893d6d335c98ecc00874d7099c145716fd
Mahiz01 <mahizsk39@gmail.com>
2024-04-11 00:48:56 +0530

    finalcommit

--- Backend/app.js
+++ Backend/app.js
@@ -18,13 +18,13 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.use("/",(req,res)=>{
-    res.json({
-        success:true,
-    message:"U R ALL DONE NOW"
-    })
+// app.use("/",(req,res)=>{
+//     res.json({
+//         success:true,
+//     message:"U R ALL DONE NOW"
+//     })
     
-})
+// })
 
 app.use("/api/v1/message",messageRoutes);
 
--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://tiny-blue-coral-veil.cyclic.app/api/v1/message/send",
         {
           name,
           email,
